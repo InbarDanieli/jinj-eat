d80bf1bd8d1f36d771d44747b98d99c1a3e22d3b
InbarDanieli <inbar0308@gmail.com>
2022-05-12 16:58:37 +0300

    fix: ui + calendar navigation with day component

--- src/component/tracker/month/Month.js
+++ src/component/tracker/month/Month.js
@@ -1,25 +1,32 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Calendar from 'react-calendar';
 import InfoContext from '../../../ContextInfo';
 import './Month.css';
+import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
 
 function Month() {
   const { date, setDate } = useContext(InfoContext)
+  const navigate = useNavigate()
 
-  
+
+  console.log(moment(date).daysInMonth().toString());
+  console.log(moment(date).startOf("month").toString());
+  console.log(moment(date).calendar());
+
   return (
     <div>
       <Calendar
-      tileContent	={()=>{}}
-      // showWeekNumbers = {true}
-      showNeighboringMonth	= {false}
-      view="month"
-        navigationLabel={null}
+        tileContent={() => { }}
+        showNeighboringMonth={false}
+        view="month"
+        minDetail="month"
+        maxDetail="month"
         calendarType='Hebrew'
         prev2Label={null}
         next2Label={null}
         onChange={setDate}
-        onClickDay={(e)=>{console.log(e);}}
+        onClickDay={(e) => { navigate("./day"); setDate(e); }}
         value={date}
       />
     </div>
